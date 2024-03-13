d6dfd9d960995dabca2173383183788218c3bfbe
Kacpermisz <kacpermisz0@gmail.com>
2024-03-13 10:57:24 +0100

    dodanie kwadratu

---
+++ exercise1/prostokat.py
@@ -0,0 +1,10 @@
+import math
+
+print("Podaj dlugosc krawedzi a")
+x=input()
+print("Podaj dlugosc krawedzi b")
+y=input()
+
+prostokat= float(x)*float(y)
+
+print("Pole prostokatu wynosi: " + str(prostokat))
